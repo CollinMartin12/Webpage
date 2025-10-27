5b66ae17cb7fe2ae42d547b17ad8a7a9b05c7e9c
Collin Martin <collin@Collins-MacBook-Air.local>
2025-10-27 11:55:46 +0100

    Auth Working + Database of users and posts from microblog

---
+++ microblog/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_login import LoginManager
+
+# Declarations to insert before the create_app function:
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    # A secret for signing session cookies
+    app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
+    
+    # Code to place inside create_app, after the other app.config assignment
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "mysql+pymysql://26_webapp_29:mx4ZAdkY@mysql.lab.it.uc3m.es/26_webapp_29a"
+
+    db.init_app(app)
+    
+    # Initialize Flask-Login
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    # Register blueprints
+    # (we import main from here to avoid circular imports in the next lab)
+    from . import main
+    from . import auth
+    from . import model
+
+    app.register_blueprint(main.bp)
+    app.register_blueprint(auth.bp)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.get(model.User, int(user_id))
+    
+    return app
---
+++ microblog/main.py
@@ -0,0 +1,22 @@
+import datetime
+import dateutil.tz
+
+from flask import Blueprint, render_template
+import flask_login
+
+
+from . import model
+
+bp = Blueprint("main", __name__)
+
+
+@bp.route("/")
+@flask_login.login_required
+def index():
+    user = model.User(id=1, email="mary@example.com", name="mary", password="dummy", description="")
+    posts = [
+        model.Post(id=1, user_id=user.id, text="Test post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())),
+        model.Post(id=2, user_id=user.id, text="Another post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())),
+    ]
+    return render_template("main/index.html", posts=posts)
+
