a6cec12935a362b59840b933de18f660c43fe18a
Collin Martin <collin@Collins-MacBook-Air.local>
2025-11-03 17:02:29 +0100

    Adding Model

--- microblog/model.py
+++ microblog/model.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 import flask_login
 
+
 from . import db
 
 
@@ -14,34 +15,70 @@ class User(flask_login.UserMixin, db.Model):
     email: Mapped[str] = mapped_column(String(128), unique=True)
     name: Mapped[str] = mapped_column(String(64))
     password: Mapped[str] = mapped_column(String(256))
-    posts: Mapped[List["Post"]] = relationship(back_populates="user")
     description: Mapped[Optional[str]] = mapped_column(String(512), nullable=True)
-    messages: Mapped[List["Message"]] = relationship(back_populates="user")
-    trips: Mapped[List["Trip"]] = relationship(back_populates="user")
-    meetups: Mapped[List["Meetups"]] = relationship(back_populates="user")
+    profile_picture: Mapped[Optional[str]] = mapped_column(String(512), nullabel = True)
 
+    city_id: Mapped[int] = mapped_column(ForeignKey("city.id"), index=True)
+    neightborhood_id = Mapped[int] = mapped_column(ForeignKey{"neightborhood_id"}, nullable = True)
 
-class Post(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="posts")
-    text: Mapped[str] = mapped_column(String(512))
-    timestamp: Mapped[datetime.datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
+    city: Mapped[Optional["City"]] = relationship()
+    neighborhood: Mapped[Optional["Neighborhood"]] = relationship()
+
+    interests: Mapped[List["Interest"]] = relationship(secondary=user_interest, lazy="selectin")
+
+    following: Mapped[List["User"]] = relationship(
+        "User",
+        secondary=user_follow,
+        primaryjoin=id == user_follow.c.follower_id,
+        secondaryjoin=id == user_follow.c.followed_id,
+        backref="followers",
+        lazy="selectin",
     )
-    trip_id: Mapped[int] = mapped_column(ForeignKey("trip.id"))
-    trip: Mapped["Trip"] = relationship(back_populates="posts")
-    messages: Mapped[List["Message"]] = relationship(back_populates="post")
-    # response_to_id: Mapped[Optional[int]] = mapped_column(ForeignKey("post.id"))
-    # response_to: Mapped["Post"] = relationship(
-    #     back_populates="responses", remote_side=[id]
-    # )
+
+    trip_posts: Mapped[List["TripPost"]] = relationship(back_populates="author")
+    trip_comments: Mapped[List["TripComment"]] = relationship(back_populates="author")
+
+    trip_participations: Mapped[List["TripParticipant"]] = relationship(back_populates="user")
+    meetups_hosted: Mapped[List["Meetup"]] = relationship(back_populates="host")
+    meetups_joined: Mapped[List["Meetup"]] = relationship(
+        secondary=meetup_participant, back_populates="participants", lazy="selectin"
+    )
+    
+        # DMs
+    conversations: Mapped[List["Conversation"]] = relationship(
+        secondary=conversation_participant, back_populates="participants", lazy="selectin"
+    )
+    direct_messages_sent: Mapped[List["DirectMessage"]] = relationship(back_populates="sender")
+
 
 
 class Trip(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="trips")
+    creator_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
+    creator: Mapped["User"] = relationship(back_populates="trips_created")
+
+    # DESTINATIONS
+    departure_city_id: Mapped[Optional[int]] = mapped_column(ForeignKey("city.id"), nullable=True, index=True)
+    departure_neighborhood_id: Mapped[Optional[int]] = mapped_column(ForeignKey("neighborhood.id"), nullable=True, index=True)
+    destination_city_id: Mapped[Optional[int]] = mapped_column(ForeignKey("city.id"), nullable=False, index=True)
+    destination_neighborhood_id: Mapped[Optional[int]] = mapped_column(ForeignKey("neighborhood.id"), nullable=True, index=True)
+    
+    departure_city: Mapped[Optional["City"]] = relationship(foreign_keys=[departure_city_id])
+    departure_neighborhood: Mapped[Optional["Neighborhood"]] = relationship(foreign_keys=[departure_neighborhood_id])
+    destination_city: Mapped[Optional["City"]] = relationship(foreign_keys=[destination_city_id])
+    destination_neighborhood: Mapped[Optional["Neighborhood"]] = relationship(foreign_keys=[destination_neighborhood_id])
+
+    # TIMING
+    definite_date: Mapped[Optional[dt.date]] = mapped_column(Date, nullable=True)
+    status: Mapped[Optional[str]] = mapped_column(TripStatus, default="PLANNING", index=True)
+    status_time: Mapped[dt.datetime] = mapped_column(DateTime(timezone=True), default=dt.datetime.utcnow, index=True)
+
+
+    budget: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    description: Mapped[str] = mapped_column(Text)  # longer than 512 in practice
+    restaurant_name: Mapped[Optional[str]] = mapped_column(String(256), nullable=True)
+
+
     departure: Mapped[str] = mapped_column(String(128))
     destination: Mapped[str] = mapped_column(String(128))
     possible_dates: Mapped[datetime.date] = mapped_column(Date)
@@ -90,3 +127,4 @@ class Meetups(db.Model):
     date: Mapped[datetime.date] = mapped_column(Date)
     time: Mapped[datetime.time] = mapped_column(Time)
     status: Mapped[str] = mapped_column(String(128))
+
